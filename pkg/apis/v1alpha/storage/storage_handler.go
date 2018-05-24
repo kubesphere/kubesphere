025035e884f8baa30b897d0d29d673095ea519c6
wnxn <wangx201401@126.com>
2018-05-24 16:17:27 +0800

    Add the API of listing all PVC of the specific storageclass

---
+++ pkg/apis/v1alpha/storage/storage_handler.go
@@ -0,0 +1,14 @@
+package storage
+
+import (
+	"github.com/emicklei/go-restful"
+	"kubesphere.io/kubesphere/pkg/filter/route"
+	"kubesphere.io/kubesphere/pkg/models"
+)
+
+func Register(ws *restful.WebService, subPath string) {
+	ws.Route(ws.GET(subPath+"/storageclasses/{storageclass}/persistentvolumeclaims").
+		To(models.GetStorageListClaims).Filter(route.RouteLogging)).
+		Consumes(restful.MIME_JSON, restful.MIME_XML).
+		Produces(restful.MIME_JSON)
+}
