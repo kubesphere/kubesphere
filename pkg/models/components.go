49693ed8b9dd75419c150f2f883bd6fbc1af4d25
yanmingfan <yanmingfan@yunify.com>
2018-06-05 20:59:28 +0800

    add kubesphere components

--- pkg/models/components.go
+++ pkg/models/components.go
@@ -29,6 +29,7 @@ import (
 const KUBESYSTEM = "kube-system"
 const OPENPITRIX = "openpitrix-system"
 const ISTIO  = "istio-system"
+const KUBESPHERE  = "kubesphere-system"
 
 type Components struct {
 	Name         string      `json:"name"`
@@ -229,9 +230,9 @@ func GetComponents() (result []Components, err error) {
 
 	}
 
-	templates = []string{"kube-dns", "heapster", "monitoring-influxdb", "iam", "openpitrix","istio"}
+	templates = []string{"kube-dns", "heapster", "monitoring-influxdb", "iam", "openpitrix","istio","kubesphere"}
 
-	namespaces := []string{KUBESYSTEM, OPENPITRIX,ISTIO}
+	namespaces := []string{KUBESYSTEM, OPENPITRIX,ISTIO,KUBESPHERE}
 
 	for _, ns := range namespaces {
 
