c3aaddf68643d66574d53b40f78e5437ab2be85d
runzexia <runzexia@yunify.com>
2019-05-13 10:43:00 +0800

    update

--- cmd/ks-iam/app/server.go
+++ cmd/ks-iam/app/server.go
@@ -18,7 +18,6 @@
 package app
 
 import (
-	"bytes"
 	goflag "flag"
 	"fmt"
 	"github.com/golang/glog"
@@ -29,13 +28,13 @@ import (
 	"kubesphere.io/kubesphere/pkg/filter"
 	"kubesphere.io/kubesphere/pkg/informers"
 	"kubesphere.io/kubesphere/pkg/models/iam"
+	"kubesphere.io/kubesphere/pkg/server"
 	"kubesphere.io/kubesphere/pkg/signals"
 	"kubesphere.io/kubesphere/pkg/simple/client/admin_jenkins"
 	"kubesphere.io/kubesphere/pkg/simple/client/devops_mysql"
 	"kubesphere.io/kubesphere/pkg/utils/jwtutil"
 	"log"
 	"net/http"
-	goRuntime "runtime"
 	"time"
 )
 
@@ -86,7 +85,7 @@ func Run(s *options.ServerRunOptions) error {
 	container := runtime.Container
 	container.Filter(filter.Logging)
 	container.DoNotRecover(false)
-	container.RecoverHandler(logStackOnRecover)
+	container.RecoverHandler(server.LogStackOnRecover)
 
 	for _, webservice := range container.RegisteredWebServices() {
 		for _, route := range webservice.Routes() {
@@ -135,19 +134,4 @@ func initializeAdminJenkins() {
 
 func initializeDevOpsDatabase() {
 	devops_mysql.OpenDatabase()
-}
-
-func logStackOnRecover(panicReason interface{}, httpWriter http.ResponseWriter) {
-	var buffer bytes.Buffer
-	buffer.WriteString(fmt.Sprintf("recover from panic situation: - %v\r\n", panicReason))
-	for i := 2; ; i += 1 {
-		_, file, line, ok := goRuntime.Caller(i)
-		if !ok {
-			break
-		}
-		buffer.WriteString(fmt.Sprintf("    %s:%d\r\n", file, line))
-	}
-	glog.Error(buffer.String())
-	httpWriter.WriteHeader(http.StatusInternalServerError)
-	httpWriter.Write([]byte("recover from panic situation"))
 }
