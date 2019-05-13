43455761c659ba7a4d9766b61c0d6d3af2b14f5b
Jeff <jeffzhang@yunify.com>
2019-05-13 13:53:40 +0800

    fix router update bug

--- pkg/controller/add_strategy.go
+++ pkg/controller/add_strategy.go
@@ -21,9 +21,6 @@ import (
 )
 
 func init() {
-	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
-	//AddToManagerFuncs = append(AddToManagerFuncs, strategy.Add)
-
 	// Add application to manager functions
 	AddToManagerFuncs = append(AddToManagerFuncs, application.Add)
 
