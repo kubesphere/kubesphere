bf366913d0894998d0fcd0baabad83278ea1e72d
soulseen <sunzhu@yunify.com>
2019-09-18 14:57:15 +0800

    fix cronjob msg parse
    Signed-off-by: soulseen <sunzhu@yunify.com>

--- pkg/models/devops/devops_test.go
+++ pkg/models/devops/devops_test.go
@@ -1,6 +1,8 @@
 package devops
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_parseCronJobTime(t *testing.T) {
 	type Except struct {
@@ -16,6 +18,8 @@ func Test_parseCronJobTime(t *testing.T) {
 		{"上次运行的时间 Thursday, January 3, 2019 11:56:30 PM UTC; 下次运行的时间 Friday, January 3, 2020 12:11:30 AM UTC.", Except{Last: "2019-01-03T23:56:30Z", Next: "2020-01-03T00:11:30Z"}},
 		{"上次运行的时间 Tuesday, September 10, 2019 8:41:34 AM UTC; 下次运行的时间 Tuesday, September 10, 2019 9:41:34 AM UTC.", Except{Last: "2019-09-10T08:41:34Z", Next: "2019-09-10T09:41:34Z"}},
 		{"上次运行的时间 Tuesday, September 10, 2019 9:15:26 AM UTC; 下次运行的时间 Tuesday, September 10, 2019 10:03:26 AM UTC.", Except{Last: "2019-09-10T09:15:26Z", Next: "2019-09-10T10:03:26Z"}},
+		{"Would last have run at Tuesday, September 10, 2019 9:15:26 AM UTC; would next run at Tuesday, September 10, 2019 10:03:26 AM UTC.", Except{Last: "2019-09-10T09:15:26Z", Next: "2019-09-10T10:03:26Z"}},
+		{"Would last have run at Tuesday, September 10, 2019 8:41:34 AM UTC; would next run at Tuesday, September 10, 2019 9:41:34 AM UTC.", Except{Last: "2019-09-10T08:41:34Z", Next: "2019-09-10T09:41:34Z"}},
 	}
 
 	for _, item := range Items {
