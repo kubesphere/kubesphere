aee01ccd0ba774da9b7ab7ee7e317669f28da4a4
Jeff <jeffzhang@yunify.com>
2019-09-25 14:50:58 +0800

    fix ks account options

--- cmd/ks-iam/app/options/options.go
+++ cmd/ks-iam/app/options/options.go
@@ -18,12 +18,19 @@
 package options
 
 import (
-	"github.com/spf13/pflag"
+	"flag"
+	cliflag "k8s.io/component-base/cli/flag"
+	"k8s.io/klog"
 	genericoptions "kubesphere.io/kubesphere/pkg/server/options"
+	"kubesphere.io/kubesphere/pkg/simple/client/k8s"
+	"kubesphere.io/kubesphere/pkg/simple/client/ldap"
+	"strings"
 )
 
 type ServerRunOptions struct {
 	GenericServerRunOptions *genericoptions.ServerRunOptions
+	KubernetesOptions       *k8s.KubernetesOptions
+	LdapOptions             *ldap.LdapOptions
 	AdminEmail              string
 	AdminPassword           string
 	TokenExpireTime         string
@@ -34,15 +41,33 @@ type ServerRunOptions struct {
 func NewServerRunOptions() *ServerRunOptions {
 	s := &ServerRunOptions{
 		GenericServerRunOptions: genericoptions.NewServerRunOptions(),
+		KubernetesOptions:       k8s.NewKubernetesOptions(),
+		LdapOptions:             ldap.NewLdapOptions(),
 	}
 	return s
 }
 
-func (s *ServerRunOptions) AddFlags(fs *pflag.FlagSet) {
+func (s *ServerRunOptions) Flags() (fss cliflag.NamedFlagSets) {
+
+	fs := fss.FlagSet("generic")
+
 	fs.StringVar(&s.AdminEmail, "admin-email", "admin@kubesphere.io", "default administrator's email")
 	fs.StringVar(&s.AdminPassword, "admin-password", "passw0rd", "default administrator's password")
 	fs.StringVar(&s.TokenExpireTime, "token-expire-time", "2h", "token expire time,valid time units are \"ns\",\"us\",\"ms\",\"s\",\"m\",\"h\"")
 	fs.StringVar(&s.JWTSecret, "jwt-secret", "", "jwt secret")
 	fs.StringVar(&s.AuthRateLimit, "auth-rate-limit", "5/30m", "specifies the maximum number of authentication attempts permitted and time interval,valid time units are \"s\",\"m\",\"h\"")
+
 	s.GenericServerRunOptions.AddFlags(fs)
+
+	s.KubernetesOptions.AddFlags(fss.FlagSet("kubernetes"))
+
+	kfs := fss.FlagSet("klog")
+	local := flag.NewFlagSet("klog", flag.ExitOnError)
+	klog.InitFlags(local)
+	local.VisitAll(func(fl *flag.Flag) {
+		fl.Name = strings.Replace(fl.Name, "_", "-", -1)
+		kfs.AddGoFlag(fl)
+	})
+
+	return fss
 }
