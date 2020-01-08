7e6770e0f4170d4c141884fbc184b16c5cc71b9e
runzexia <runzexia@yunify.com>
2020-01-08 17:21:08 +0800

    clean
    Signed-off-by: runzexia <runzexia@yunify.com>

--- pkg/simple/client/devops/interface.go
+++ pkg/simple/client/devops/interface.go
@@ -1,47 +1,6 @@
 package devops
 
-import (
-	"net/http"
-)
-
 type Interface interface {
-	SendJenkinsRequest(baseUrl string, req *http.Request) ([]byte, error)
-
-	SendJenkinsRequestWithHeaderResp(baseUrl string, req *http.Request) ([]byte, http.Header, error)
-
-	//ValidateJenkinsfile(jenkinsfile string) (*jenkins.ValidateJenkinsfileResponse, error)
-	//
-	//ValidatePipelineJson(json string) (*jenkins.ValidatePipelineJsonResponse, error)
-	//
-	//PipelineJsonToJenkinsfile(json string) (*jenkins.PipelineJsonToJenkinsfileResponse, error)
-	//
-	//JenkinsfileToPipelineJson(jenkinsfile string) (*jenkins.JenkinsfileToPipelineJsonResponse, error)
-	//
-	//CreateFolder(name, description string, parents ...string) (*jenkins.Folder, error)
-	//
-	//CreateJobInFolder(config string, jobName string, parentIDs ...string) (*jenkins.Job, error)
-	//
-	//DeleteJob(name string, parentIDs ...string) (bool, error)
-	//
-	//BuildJob(name string, options ...interface{}) (int64, error)
-	//
-	//GetBuild(jobName string, number int64) (*jenkins.Build, error)
-	//
-	//GetJob(id string, parentIDs ...string) (*jenkins.Job, error)
-	//
-	//GetFolder(id string, parents ...string) (*jenkins.Folder, error)
-	//
-	//GetGlobalRole(roleName string) (*jenkins.GlobalRole, error)
-	//
-	//GetProjectRole(roleName string) (*jenkins.ProjectRole, error)
-	//
-	//AddGlobalRole(roleName string, ids jenkins.GlobalPermissionIds, overwrite bool) (*jenkins.GlobalRole, error)
-	//
-	//DeleteProjectRoles(roleName ...string) error
-	//
-	//AddProjectRole(roleName string, pattern string, ids jenkins.ProjectPermissionIds, overwrite bool) (*jenkins.ProjectRole, error)
-
-	DeleteUserInProject(username string) error
 
 	CredentialOperator
 
@@ -53,19 +12,4 @@ type Interface interface {
 
 	ProjectPipelineOperator
 
-	//AdapterOperator
-}
-
-type AdapterOperator interface {
-	//SendPureRequest(path string, req *http.Request) ([]byte, error)
-
-	//SendRequestWithHeaderResp(path string, req *http.Request) ([]byte, http.Header, error)
-
-	// 	ValidateJenkinsfile(jenkinsfile string) (*jenkins.ValidateJenkinsfileResponse, error)
-
-	// 	ValidatePipelineJson(json string) (*jenkins.ValidatePipelineJsonResponse, error)
-
-	// 	PipelineJsonToJenkinsfile(json string) (*jenkins.PipelineJsonToJenkinsfileResponse, error)
-
-	// 	JenkinsfileToPipelineJson(jenkinsfile string) (*jenkins.JenkinsfileToPipelineJsonResponse, error)
 }
