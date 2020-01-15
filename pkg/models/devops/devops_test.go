7b6d6746c862a46880beac4f36b2d9feaa37b843
zhuxiaoyang <sunzhu@yunify.com>
2020-01-15 17:00:22 +0800

    status
    Signed-off-by: zhuxiaoyang <sunzhu@yunify.com>

--- pkg/models/devops/devops_test.go
+++ pkg/models/devops/devops_test.go
@@ -11,16 +11,19 @@ const baseUrl = "http://127.0.0.1/kapis/devops.kubesphere.io/v1alpha2/"
 
 func TestGetNodesDetail(t *testing.T) {
 	PipelineRunNodes := []devops.PipelineRunNodes{
-		{},
-		{},
-		{},
+		{ID: "0", Result: "true", DisplayName: "fakeBranchPipelineRunNode1"},
+		{ID: "1", Result: "true", DisplayName: "fakeBranchPipelineRunNode2"},
+		{ID: "2", Result: "true", DisplayName: "fakeBranchPipelineRunNode3"},
 	}
 
 	NodeSteps := []devops.NodeSteps{
-
+		{ID:"1",Result:"true",DisplayName:"fakeBranchNodeStep"},
 	}
 
-	fakeDevops := fake.NewFakeDevopsNodesDetail(PipelineRunNodes,NodeSteps)
+	//dict := make(map[string][]devops.NodeSteps)
+	//dict["1"] = NodeSteps
+
+	fakeDevops := fake.NewFakeDevopsNodesDetail(PipelineRunNodes, NodeSteps)
 
 	devopsOperator := NewDevopsOperator(fakeDevops)
 
@@ -31,28 +34,44 @@ func TestGetNodesDetail(t *testing.T) {
 		t.Fatalf("should not get error %+v", err)
 	}
 
-	for _, v := range nodesDetails{
-		if v.ID != v.Steps[0].ID {
+	for i, v := range nodesDetails {
+		if v.ID != string(i) {
 			t.Fatalf("Node id %s and step od %s should equal", v.ID, v.Steps[0].ID)
+		}
+		if v.Steps[0].ID != NodeSteps[0].ID {
+			t.Fatalf("Get step id %s but intput is %s.", v.Steps[0].ID, NodeSteps[0].ID)
 		}
 	}
 }
 
 func TestGetBranchNodesDetail(t *testing.T) {
-	fakeDevops := fake.NewFakeDevopsNodesDetail()
+	PipelineRunNodes := []devops.PipelineRunNodes{
+		{ID: "1", Result: "true", DisplayName: "fakeBranchPipelineRunNode1"},
+		{ID: "2", Result: "true", DisplayName: "fakeBranchPipelineRunNode2"},
+		{ID: "3", Result: "true", DisplayName: "fakeBranchPipelineRunNode3"},
+	}
+
+	NodeSteps := []devops.NodeSteps{
+		{ID:"1",Result:"true",DisplayName:"fakeBranchNodeStep"},
+	}
+
+	fakeDevops := fake.NewFakeDevopsNodesDetail(PipelineRunNodes, NodeSteps)
 
 	devopsOperator := NewDevopsOperator(fakeDevops)
 
-	httpReq, _ := http.NewRequest(http.MethodGet, baseUrl+"devops/project/pipelines/pipeline/branches/branch/runs/run/nodesdetail/?limit=10000", nil)
+	httpReq, _ := http.NewRequest(http.MethodGet, baseUrl+"devops/project/pipelines/pipeline/branchs/branc/runs/run/nodesdetail/?limit=10000", nil)
 
 	nodesDetails, err := devopsOperator.GetBranchNodesDetail("project", "pipeline","branch", "run", httpReq)
 	if err != nil || nodesDetails == nil {
 		t.Fatalf("should not get error %+v", err)
 	}
 
-	for _, v := range nodesDetails{
-		if v.ID != v.Steps[0].ID {
+	for i, v := range nodesDetails {
+		if v.ID != string(i) {
 			t.Fatalf("Node id %s and step od %s should equal", v.ID, v.Steps[0].ID)
+		}
+		if v.Steps[0].ID != NodeSteps[0].ID {
+			t.Fatalf("Get step id %s but intput is %s.", v.Steps[0].ID, NodeSteps[0].ID)
 		}
 	}
 }
