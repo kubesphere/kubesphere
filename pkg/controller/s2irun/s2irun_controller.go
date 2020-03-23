0ac15ea7443d5e5d421a386863ed698b30db642e
runzexia <runzexia@yunify.com>
2020-03-23 10:01:22 +0800

    refmt code
    Signed-off-by: runzexia <runzexia@yunify.com>

--- pkg/controller/s2irun/s2irun_controller.go
+++ pkg/controller/s2irun/s2irun_controller.go
@@ -25,8 +25,8 @@ import (
 )
 
 /**
-s2irun-controller used to handle s2irun's delete logic.
-s2irun creation and operation provided by s2ioperator
+  s2irun-controller used to handle s2irun's delete logic.
+  s2irun creation and operation provided by s2ioperator
 */
 type Controller struct {
 	client clientset.Interface
@@ -219,9 +219,9 @@ func (c Controller) syncHandler(key string) error {
 }
 
 /**
-	DeleteS2iBinary mainly cleans up two parts of S2iBinary
-	1. s2ibinary bound to s2irun
-    2. s2ibinary that has been created for more than 24 hours but has not been used
+  DeleteS2iBinary mainly cleans up two parts of S2iBinary
+  1. s2ibinary bound to s2irun
+  2. s2ibinary that has been created for more than 24 hours but has not been used
 */
 func (c Controller) DeleteS2iBinary(s2irun *devopsv1alpha1.S2iRun) error {
 	s2iBinName := s2irun.Labels[devopsv1alpha1.S2iBinaryLabelKey]
