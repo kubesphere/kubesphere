5cb1c948c4f9e4d630517f898a30b886cee1cde8
runzexia <runzexia@yunify.com>
2020-03-31 11:24:33 +0800

    add event notifier
    Signed-off-by: runzexia <runzexia@yunify.com>

--- pkg/kapis/devops/v1alpha3/event.go
+++ pkg/kapis/devops/v1alpha3/event.go
@@ -1,1 +1,140 @@
 package v1alpha3
+
+import "sync"
+
+var notifier eventNotifier
+var once sync.Once
+
+// eventNotifier is the bus for event distribution.
+// when you need to add a specific Event handler,
+// you need to implement the EventHandler interface and call RegisterEventHandler to register with eventNotifier.
+// eventNotifier should be accessed in singleton mode, do not initialize an eventNotifier instance yourself.
+
+type eventNotifier struct {
+	lock      sync.RWMutex
+	listeners []EventHandler
+}
+
+func (e *eventNotifier) onPipelineStarted(event Event) {
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+	for _, handler := range e.listeners {
+		handler.OnPipelineStarted(event)
+	}
+}
+
+func (e *eventNotifier) onPipelineCompleted(event Event) {
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+	for _, handler := range e.listeners {
+		handler.OnPipelineCompleted(event)
+	}
+}
+
+func (e *eventNotifier) onPipelineFinalized(event Event) {
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+	for _, handler := range e.listeners {
+		handler.OnPipelineFinalized(event)
+	}
+}
+
+func (e *eventNotifier) onPipelinePendingReview(event Event) {
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+	for _, handler := range e.listeners {
+		handler.OnPipelinePendingReview(event)
+	}
+}
+
+func (e *eventNotifier) onPipelineReviewProceeded(event Event) {
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+	for _, handler := range e.listeners {
+		handler.OnPipelineReviewProceeded(event)
+	}
+}
+
+func (e *eventNotifier) onPipelineReviewAborted(event Event) {
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+	for _, handler := range e.listeners {
+		handler.OnPipelineReviewAborted(event)
+	}
+}
+
+func GetEventNotifier() *eventNotifier {
+	once.Do(func() {
+		notifier = eventNotifier{
+			lock: sync.RWMutex{},
+		}
+	})
+	return &notifier
+}
+
+func RegisterEventHandler(handler EventHandler) {
+	notifier := GetEventNotifier()
+	notifier.lock.Lock()
+	defer notifier.lock.Unlock()
+	notifier.listeners = append(notifier.listeners, handler)
+}
+
+type EventHandler interface {
+	OnPipelineStarted(event Event)
+	OnPipelineCompleted(event Event)
+	OnPipelineFinalized(event Event)
+	OnPipelinePendingReview(event Event)
+	OnPipelineReviewProceeded(event Event)
+	OnPipelineReviewAborted(event Event)
+}
+
+type ResourceEventHandlerFuncs struct {
+	PipelineStartedFunc     func(event Event)
+	PipelineCompletedFunc   func(event Event)
+	PipelineFinalizedFunc   func(event Event)
+	PipelinePendingReview   func(event Event)
+	PipelineReviewProceeded func(event Event)
+	PipelineReviewAborted   func(event Event)
+}
+
+// OnPipelineStarted calls PipelineStartedFunc if it's not nil.
+func (r ResourceEventHandlerFuncs) OnPipelineStarted(event Event) {
+	if r.PipelineStartedFunc != nil {
+		r.PipelineStartedFunc(event)
+	}
+}
+
+// OnPipelineCompleted calls PipelineCompletedFunc if it's not nil.
+func (r ResourceEventHandlerFuncs) OnPipelineCompleted(event Event) {
+	if r.PipelineCompletedFunc != nil {
+		r.PipelineCompletedFunc(event)
+	}
+}
+
+// OnPipelineFinalized calls PipelineFinalizedFunc if it's not nil.
+func (r ResourceEventHandlerFuncs) OnPipelineFinalized(event Event) {
+	if r.PipelineFinalizedFunc != nil {
+		r.PipelineFinalizedFunc(event)
+	}
+}
+
+// OnPipelinePendingReview calls PipelinePendingReview if it's not nil.
+func (r ResourceEventHandlerFuncs) OnPipelinePendingReview(event Event) {
+	if r.PipelinePendingReview != nil {
+		r.PipelinePendingReview(event)
+	}
+}
+
+// PipelinePendingReview calls PipelineReviewProceeded if it's not nil.
+func (r ResourceEventHandlerFuncs) OnPipelineReviewProceeded(event Event) {
+	if r.PipelineReviewProceeded != nil {
+		r.PipelineReviewProceeded(event)
+	}
+}
+
+// onPipelineReviewAborted calls PipelineReviewAborted if it's not nil.
+func (r ResourceEventHandlerFuncs) OnPipelineReviewAborted(event Event) {
+	if r.PipelineReviewAborted != nil {
+		r.PipelineReviewAborted(event)
+	}
+}
---
+++ pkg/kapis/devops/v1alpha3/event_test.go
@@ -0,0 +1,39 @@
+package v1alpha3
+
+import (
+	"reflect"
+	"testing"
+)
+
+type recorder struct {
+	store []Event
+}
+
+func (r *recorder) record(event Event) {
+	r.store = append(r.store, event)
+}
+
+func TestRegisterEventHandler(t *testing.T) {
+	r := &recorder{}
+	RegisterEventHandler(ResourceEventHandlerFuncs{
+		PipelineStartedFunc: func(event Event) {
+			r.record(event)
+		},
+	})
+	expects := []Event{
+		{Timestamp: 1},
+		{Timestamp: 2},
+	}
+	for _, expect := range expects {
+		GetEventNotifier().onPipelineStarted(expect)
+	}
+	if len(r.store) != 2 {
+		t.Fatalf("expect store length %v, but got %v", len(expects), len(r.store))
+	}
+	for i, expect := range expects {
+		if !reflect.DeepEqual(expect, r.store[i]) {
+			t.Fatalf("expect \n %v \n, but got \n %v", expect, r.store[i])
+		}
+	}
+
+}
