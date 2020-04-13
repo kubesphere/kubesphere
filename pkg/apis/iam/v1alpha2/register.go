bb9e12be3dc7b6aca0f69213e882b557159336ef
hongming <talonwan@yunify.com>
2020-04-14 00:15:18 +0800

    fix iam admission webhook (#2008)
    Signed-off-by: hongming <talonwan@yunify.com>

--- pkg/apis/iam/v1alpha2/register.go
+++ pkg/apis/iam/v1alpha2/register.go
@@ -50,6 +50,7 @@ func Resource(resource string) schema.GroupResource {
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&User{},
+		&UserList{},
 		&Role{},
 		&RoleList{},
 		&RoleBinding{},
