17200172a8bbda9d81cdd560eb5076ee983ef2d3
zryfish <jeffzhang@yunify.com>
2020-05-18 18:31:57 +0800

    remove specific version of generic proxy server (#2084)

--- pkg/kapis/alerting/v1/register.go
+++ pkg/kapis/alerting/v1/register.go
@@ -6,7 +6,8 @@ import (
 	"kubesphere.io/kubesphere/pkg/kapis/generic"
 )
 
-var GroupVersion = schema.GroupVersion{Group: "alerting.kubesphere.io", Version: "v1"}
+// there are no versions specified cause we want to proxy all versions of requests to backend service
+var GroupVersion = schema.GroupVersion{Group: "alerting.kubesphere.io", Version: ""}
 
 func AddToContainer(container *restful.Container, endpoint string) error {
 	proxy, err := generic.NewGenericProxy(endpoint, GroupVersion.Group, GroupVersion.Version)
