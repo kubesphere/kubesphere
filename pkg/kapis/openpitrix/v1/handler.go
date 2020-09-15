fec666e696e87d6e779a1853ca9d85663912f302
李皓伟 <haoweili35@gmail.com>
2020-09-15 15:11:31 +0800

    squash err handle
    Signed-off-by: 李皓伟 <haoweili35@gmail.com>

--- pkg/kapis/openpitrix/v1/handler.go
+++ pkg/kapis/openpitrix/v1/handler.go
@@ -922,12 +922,7 @@ func handleOpenpitrixError(resp *restful.Response, err error) {
 		api.HandleNotFound(resp, nil, err)
 		return
 	}
-	if status.Code(err) == codes.InvalidArgument {
-		klog.V(4).Infoln(err)
-		api.HandleBadRequest(resp, nil, err)
-		return
-	}
-	if status.Code(err) == codes.FailedPrecondition {
+	if status.Code(err) == codes.InvalidArgument || status.Code(err) == codes.FailedPrecondition {
 		klog.V(4).Infoln(err)
 		api.HandleBadRequest(resp, nil, err)
 		return
