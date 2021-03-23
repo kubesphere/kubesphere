ab7abe5e352ee9b3f7d58aae3f099b82642d6c4f
zackzhangkai <zhangkaiamm@gmail.com>
2021-03-23 09:32:13 +0800

    fix import error
    Signed-off-by: zackzhangkai <zhangkaiamm@gmail.com>

--- pkg/tools.go
+++ pkg/tools.go
@@ -23,6 +23,6 @@ import (
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
-	_ "k8s.io/kube-openapi/cmd/openapi-gen/"
+	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
--- tools/tools.go
+++ tools/tools.go
@@ -23,5 +23,5 @@ import (
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
-	_ "k8s.io/kube-openapi/cmd/openapi-gen/"
+	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 )
