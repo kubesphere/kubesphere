f37495bc3211f43066e308679ab662c596fd04be
larryliuqing <larryliuqing@163.com>
2022-02-25 09:35:47 +0800

    Fix the "index out of range" issue when sort metrics

--- pkg/models/monitoring/sort_page.go
+++ pkg/models/monitoring/sort_page.go
@@ -120,8 +120,10 @@ func (raw *Metrics) Sort(target, order, identifier string) *Metrics {
 				// Record ordinals in the final result
 				v, ok := mv.Metadata[identifier]
 				if ok && v != "" {
-					resourceOrdinal[v] = ordinal
-					ordinal++
+					if _, ok1 := resourceOrdinal[v]; !ok1 {
+						resourceOrdinal[v] = ordinal
+						ordinal++
+					}
 				}
 			}
 		}
