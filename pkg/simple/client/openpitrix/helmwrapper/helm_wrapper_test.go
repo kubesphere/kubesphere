fe992ae53f6be7309d7b730850382eb97a32810d
nio <nioshield@gmail.com>
2022-06-27 16:44:34 +0800

    Replace the Helm command (#4852)
    * Replace the helm command line with helm action
    
    * fix log misspelling
    
    * helm wrapper formate log msg
    
    * fix: helm action faild in multi cluster

--- pkg/simple/client/openpitrix/helmwrapper/helm_wrapper_test.go
+++ pkg/simple/client/openpitrix/helmwrapper/helm_wrapper_test.go
@@ -17,9 +17,12 @@ limitations under the License.
 package helmwrapper
 
 import (
-	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
+
+	"helm.sh/helm/v3/pkg/chart"
+	"helm.sh/helm/v3/pkg/chartutil"
 
 	"kubesphere.io/kubesphere/pkg/constants"
 )
@@ -28,19 +31,44 @@ func TestHelmInstall(t *testing.T) {
 	wr := NewHelmWrapper("", "dummy", "dummy",
 		SetAnnotations(map[string]string{constants.CreatorAnnotationKey: "1234"}),
 		SetMock(true))
+	charData := GenerateChartData(t, "dummy-chart")
+	chartValues := `helm-wrapper: "test-val"`
 
-	err := wr.install("dummy-chart", "", "dummy-value", false)
+	err := wr.writeAction("dummy-chart", charData, chartValues, false)
 	if err != nil {
 		t.Fail()
 	}
 }
 
-func TestHelperProcess(t *testing.T) {
-	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-		return
+func TempDir(t *testing.T) string {
+	t.Helper()
+	d, err := ioutil.TempDir("", "kubesphere")
+	if err != nil {
+		t.Fatal(err)
+	}
+	return d
+}
+
+func GenerateChartData(t *testing.T, name string) string {
+	tmpChart := TempDir(t)
+	defer os.RemoveAll(tmpChart)
+
+	cfile := &chart.Chart{
+		Metadata: &chart.Metadata{
+			APIVersion:  chart.APIVersionV1,
+			Name:        name,
+			Description: "A Helm chart for Kubernetes",
+			Version:     "0.1.0",
+		},
 	}
 
-	// some code here to check arguments perhaps?
-	fmt.Fprintf(os.Stdout, "helm mock success")
-	os.Exit(0)
+	filename, err := chartutil.Save(cfile, tmpChart)
+	if err != nil {
+		t.Fatalf("Error creating chart for test: %v", err)
+	}
+	charData, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Fatalf("Error loading chart data %v", err)
+	}
+	return string(charData)
 }
