60432fa0b25e76512e5497b76ba923504cfddca3
Wenhao Zhou <wenhaozhou@yunify.com>
2022-09-07 16:10:26 +0800

    fix: multiple clusterrolebindings cannot autorizate

--- pkg/models/tenant/tenant.go
+++ pkg/models/tenant/tenant.go
@@ -568,10 +568,15 @@ func (t *tenantOperator) PatchWorkspaceTemplate(user user.Info, workspace string
 				return nil, err
 			}
 
+			allowd := false
 			for _, clusterRolebinding := range list.Items {
 				if clusterRolebinding.RoleRef.Name == iamv1alpha2.ClusterAdmin {
-					continue
+					allowd = true
+					break
 				}
+			}
+
+			if !allowd {
 				err = errors.NewForbidden(clusterv1alpha1.Resource(clusterv1alpha1.ResourcesPluralCluster), clusterName, fmt.Errorf(reason))
 				klog.Error(err)
 				return nil, err
