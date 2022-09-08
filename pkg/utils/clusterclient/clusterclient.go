95576bb827e70f3d5c6746324ce90f49bea91a30
Wenhao Zhou <34303854+zhou1203@users.noreply.github.com>
2022-09-08 10:23:41 +0800

    Add authorization control for patching workspacetemplates (#5191)
    * update patch workspacetemplate for supporting patch with JsonPatchType and change the authorization processing
    
    Signed-off-by: Wenhao Zhou <wenhaozhou@yunify.com>
    
    * make goimports
    
    * Fix: Of the type is not string will lead to panic
    
    Signed-off-by: Wenhao Zhou <wenhaozhou@yunify.com>
    
    * Add jsonpatchutil for handling json patch data
    
    Signed-off-by: Wenhao Zhou <wenhaozhou@yunify.com>
    
    * Updated patch workspacetemplate to to make the code run more efficiently
    
    * fix: multiple clusterrolebindings cannot autorizate
    
    * Correct wrong spelling
    
    Signed-off-by: Wenhao Zhou <wenhaozhou@yunify.com>

--- pkg/utils/clusterclient/clusterclient.go
+++ pkg/utils/clusterclient/clusterclient.go
@@ -23,6 +23,7 @@ import (
 	"sync"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
@@ -30,6 +31,7 @@ import (
 
 	clusterv1alpha1 "kubesphere.io/api/cluster/v1alpha1"
 
+	kubesphere "kubesphere.io/kubesphere/pkg/client/clientset/versioned"
 	clusterinformer "kubesphere.io/kubesphere/pkg/client/informers/externalversions/cluster/v1alpha1"
 	clusterlister "kubesphere.io/kubesphere/pkg/client/listers/cluster/v1alpha1"
 )
@@ -54,6 +56,8 @@ type ClusterClients interface {
 	GetClusterKubeconfig(string) (string, error)
 	Get(string) (*clusterv1alpha1.Cluster, error)
 	GetInnerCluster(string) *innerCluster
+	GetKubernetesClientSet(string) (*kubernetes.Clientset, error)
+	GetKubeSphereClientSet(string) (*kubesphere.Clientset, error)
 }
 
 func NewClusterClient(clusterInformer clusterinformer.ClusterInformer) ClusterClients {
@@ -181,4 +185,46 @@ func (c *clusterClients) IsHostCluster(cluster *clusterv1alpha1.Cluster) bool {
 		return true
 	}
 	return false
+}
+
+func (c *clusterClients) GetKubeSphereClientSet(name string) (*kubesphere.Clientset, error) {
+	kubeconfig, err := c.GetClusterKubeconfig(name)
+	if err != nil {
+		return nil, err
+	}
+	restConfig, err := newRestConfigFromString(kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+	clientSet, err := kubesphere.NewForConfig(restConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return clientSet, nil
+}
+
+func (c *clusterClients) GetKubernetesClientSet(name string) (*kubernetes.Clientset, error) {
+	kubeconfig, err := c.GetClusterKubeconfig(name)
+	if err != nil {
+		return nil, err
+	}
+	restConfig, err := newRestConfigFromString(kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+	clientSet, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return clientSet, nil
+}
+
+func newRestConfigFromString(kubeconfig string) (*rest.Config, error) {
+	bytes, err := clientcmd.NewClientConfigFromBytes([]byte(kubeconfig))
+	if err != nil {
+		return nil, err
+	}
+	return bytes.ClientConfig()
 }
