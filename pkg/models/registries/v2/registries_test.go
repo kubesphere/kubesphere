0f57526e0dd963d32cd9cdf0090b2efdcb142c4f
hongming <hongming@kubesphere.io>
2022-12-04 12:51:50 +0800

    Remove the broken test case

--- pkg/models/registries/v2/registries_test.go
+++ pkg/models/registries/v2/registries_test.go
@@ -102,24 +102,6 @@ func TestRegistryerListRepoTags(t *testing.T) {
 				},
 			},
 		},
-		{
-			name:      "Should fetch config from public registry with credential",
-			secret:    buildSecret("dockerhub.qingcloud.com", "guest", "guest", false),
-			image:     "dockerhub.qingcloud.com/calico/cni",
-			expectErr: false,
-			repositoryTags: RepositoryTags{
-				Registry: "dockerhub.qingcloud.com",
-				Tags: []string{
-					"v1.11.4",
-					"v3.1.3",
-					"v3.3.2",
-					"v3.3.3",
-					"v3.3.6",
-					"v3.7.3",
-					"v3.8.4",
-				},
-			},
-		},
 	}
 
 	for _, testCase := range testCases {
