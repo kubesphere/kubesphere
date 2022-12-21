efcd4c551233a38fe0da1d509cd945aa3b6e71c5
hongzhouzi <36416216+hongzhouzi@users.noreply.github.com>
2022-12-21 10:49:53 +0800

    fix: Resolved some data out of sync after live-reload. (#5422)
    refactor: Data not synchronized due to using singleton mode after live-reload.
    
    Signed-off-by: hongzhouzi <hongzhouzi@kubesphere.io>
    
    Signed-off-by: hongzhouzi <hongzhouzi@kubesphere.io>

--- pkg/simple/client/ldap/ldap.go
+++ pkg/simple/client/ldap/ldap.go
@@ -20,7 +20,6 @@ import (
 	"fmt"
 	"sort"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/go-ldap/ldap"
@@ -59,8 +58,6 @@ type ldapInterfaceImpl struct {
 	groupSearchBase string
 	managerDN       string
 	managerPassword string
-
-	once sync.Once
 }
 
 var _ Interface = &ldapInterfaceImpl{}
@@ -91,7 +88,6 @@ func NewLdapClient(options *Options, stopCh <-chan struct{}) (Interface, error) 
 		groupSearchBase: options.GroupSearchBase,
 		managerDN:       options.ManagerDN,
 		managerPassword: options.ManagerPassword,
-		once:            sync.Once{},
 	}
 
 	go func() {
@@ -99,9 +95,7 @@ func NewLdapClient(options *Options, stopCh <-chan struct{}) (Interface, error) 
 		client.close()
 	}()
 
-	client.once.Do(func() {
-		_ = client.createSearchBase()
-	})
+	_ = client.createSearchBase()
 
 	return client, nil
 }
