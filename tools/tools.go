3bbc9e92a1f8e8a8913ad28baf2f85221320dcc6
Xinzhao Xu <z2d@jifangcheng.com>
2023-04-13 13:59:42 +0800

    Merge multiple duplicate tools files (#5637)

--- tools/tools.go
+++ tools/tools.go
@@ -25,4 +25,5 @@ import (
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
