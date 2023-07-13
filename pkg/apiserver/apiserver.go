6e0a48c55586d90a627471c5a89ed7509bd963ad
LQBing <6857508+LQBing@users.noreply.github.com>
2023-07-13 11:02:58 +0800

    Use autoscaling/v2 instead of autoscaling/v2beta2 (#5833)

--- pkg/apiserver/apiserver.go
+++ pkg/apiserver/apiserver.go
@@ -466,7 +466,7 @@ func (s *APIServer) waitForResourceSync(ctx context.Context) error {
 			"ingresses",
 			"networkpolicies",
 		},
-		{Group: "autoscaling", Version: "v2beta2"}: {
+		{Group: "autoscaling", Version: "v2"}: {
 			"horizontalpodautoscalers",
 		},
 	}
