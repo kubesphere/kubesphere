23ad18296891250de5b4c2f8abebdf43c0aaed47
lingbo <lingbo@lingbohome.com>
2024-09-23 16:49:16 +0800

    feat: Adapt to oci-based helmchart repo

--- pkg/simple/client/application/oci.go
+++ pkg/simple/client/application/oci.go
@@ -3,6 +3,7 @@ package application
 import (
 	"context"
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -16,7 +17,32 @@ import (
 	"kubesphere.io/kubesphere/pkg/simple/client/oci"
 )
 
-func LoadRepoIndexFormOci(u string, cred appv2.RepoCredential) (idx helmrepo.IndexFile, err error) {
+func HelmPullFromOci(u string, cred appv2.RepoCredential) ([]byte, error) {
+	if !registry.IsOCI(u) {
+		return nil, fmt.Errorf("invalid oci URL format: %s", u)
+	}
+	_, err := url.Parse(u)
+	if err != nil {
+		klog.Errorf("invalid oci chart URL format: %s, err:%v", u, err)
+		return nil, err
+	}
+
+	client, err := newOCIRegistryClient(u, cred)
+	if err != nil {
+		return nil, err
+	}
+
+	pullRef := strings.TrimPrefix(u, fmt.Sprintf("%s://", registry.OCIScheme))
+	pullResult, err := client.Pull(pullRef)
+	if err != nil {
+		klog.Errorf("An error occurred to pull chart from repository: %s,err:%v", pullRef, err)
+		return nil, err
+	}
+
+	return pullResult.Chart.Data, nil
+}
+
+func LoadRepoIndexFromOci(u string, cred appv2.RepoCredential) (idx helmrepo.IndexFile, err error) {
 	if !registry.IsOCI(u) {
 		return idx, fmt.Errorf("invalid oci URL format: %s", u)
 	}
@@ -27,39 +53,7 @@ func LoadRepoIndexFormOci(u string, cred appv2.RepoCredential) (idx helmrepo.Ind
 		return idx, err
 	}
 
-	skipTLS := true
-	if cred.InsecureSkipTLSVerify != nil && !*cred.InsecureSkipTLSVerify {
-		skipTLS = false
-	}
-
-	reg, err := oci.NewRegistry(parsedURL.Host,
-		oci.WithTimeout(5*time.Second),
-		oci.WithBasicAuth(cred.Username, cred.Password),
-		oci.WithInsecureSkipVerifyTLS(skipTLS))
-	if err != nil {
-		return idx, err
-	}
-
-	ctx := context.Background()
-
-	repoPath := strings.TrimSuffix(parsedURL.Path, "/")
-	repoPath = strings.TrimPrefix(repoPath, "/")
-	var repoCharts []string
-	err = reg.Repositories(ctx, "", func(repos []string) error {
-		cutPrefix := repoPath
-		if cutPrefix != "" {
-			cutPrefix = cutPrefix + "/"
-		}
-		for _, repo := range repos {
-			if subRepo, found := strings.CutPrefix(repo, cutPrefix); found && subRepo != "" {
-				if !strings.Contains(subRepo, "/") {
-					repoCharts = append(repoCharts, fmt.Sprintf("%s/%s", repoPath, subRepo))
-				}
-			}
-		}
-		return nil
-	})
-
+	repoCharts, err := GetRepoChartsFromOci(parsedURL, cred)
 	if err != nil {
 		return idx, err
 	}
@@ -67,34 +61,9 @@ func LoadRepoIndexFormOci(u string, cred appv2.RepoCredential) (idx helmrepo.Ind
 		return idx, nil
 	}
 
-	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: skipTLS},
-		Proxy:           http.ProxyFromEnvironment,
-	}
-
-	opts := []registry.ClientOption{registry.ClientOptHTTPClient(&http.Client{
-		Transport: transport,
-		Timeout:   5 * time.Second,
-	})}
-
-	if reg.PlainHTTP {
-		opts = append(opts, registry.ClientOptPlainHTTP())
-	}
-
-	client, err := registry.NewClient(opts...)
-
+	client, err := newOCIRegistryClient(u, cred)
 	if err != nil {
 		return idx, err
-	}
-
-	if cred.Username != "" || cred.Password != "" {
-		err = client.Login(parsedURL.Host,
-			registry.LoginOptBasicAuth(cred.Username, cred.Password),
-			registry.LoginOptInsecure(reg.PlainHTTP))
-
-		if err != nil {
-			return idx, err
-		}
 	}
 
 	index := helmrepo.NewIndexFile()
@@ -126,5 +95,104 @@ func LoadRepoIndexFormOci(u string, cred appv2.RepoCredential) (idx helmrepo.Ind
 		}
 	}
 
+	index.SortEntries()
+
 	return *index, nil
+}
+
+func GetRepoChartsFromOci(parsedURL *url.URL, cred appv2.RepoCredential) ([]string, error) {
+	if parsedURL == nil {
+		return nil, errors.New("missing parsedURL")
+	}
+
+	skipTLS := true
+	if cred.InsecureSkipTLSVerify != nil && !*cred.InsecureSkipTLSVerify {
+		skipTLS = false
+	}
+
+	reg, err := oci.NewRegistry(parsedURL.Host,
+		oci.WithTimeout(5*time.Second),
+		oci.WithBasicAuth(cred.Username, cred.Password),
+		oci.WithInsecureSkipVerifyTLS(skipTLS))
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+
+	repoPath := strings.TrimSuffix(parsedURL.Path, "/")
+	repoPath = strings.TrimPrefix(repoPath, "/")
+	var repoCharts []string
+	err = reg.Repositories(ctx, "", func(repos []string) error {
+		cutPrefix := repoPath
+		if cutPrefix != "" {
+			cutPrefix = cutPrefix + "/"
+		}
+		for _, repo := range repos {
+			if subRepo, found := strings.CutPrefix(repo, cutPrefix); found && subRepo != "" {
+				if !strings.Contains(subRepo, "/") {
+					repoCharts = append(repoCharts, fmt.Sprintf("%s/%s", repoPath, subRepo))
+				}
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return repoCharts, nil
+}
+
+func newOCIRegistryClient(u string, cred appv2.RepoCredential) (*registry.Client, error) {
+	parsedURL, err := url.Parse(u)
+	if err != nil {
+		klog.Errorf("invalid oci repo URL format: %s, err:%v", u, err)
+		return nil, err
+	}
+
+	skipTLS := true
+	if cred.InsecureSkipTLSVerify != nil && !*cred.InsecureSkipTLSVerify {
+		skipTLS = false
+	}
+
+	reg, err := oci.NewRegistry(parsedURL.Host,
+		oci.WithTimeout(5*time.Second),
+		oci.WithBasicAuth(cred.Username, cred.Password),
+		oci.WithInsecureSkipVerifyTLS(skipTLS))
+	if err != nil {
+		return nil, err
+	}
+
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: skipTLS},
+		Proxy:           http.ProxyFromEnvironment,
+	}
+
+	opts := []registry.ClientOption{registry.ClientOptHTTPClient(&http.Client{
+		Transport: transport,
+		Timeout:   5 * time.Second,
+	})}
+
+	if reg.PlainHTTP {
+		opts = append(opts, registry.ClientOptPlainHTTP())
+	}
+
+	client, err := registry.NewClient(opts...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if cred.Username != "" || cred.Password != "" {
+		err = client.Login(parsedURL.Host,
+			registry.LoginOptBasicAuth(cred.Username, cred.Password),
+			registry.LoginOptInsecure(reg.PlainHTTP))
+
+		if err != nil {
+			return nil, err
+		}
+	}
+	return client, nil
 }
--- pkg/simple/client/application/oci_test.go
+++ pkg/simple/client/application/oci_test.go
@@ -6,16 +6,16 @@ import (
 	appv2 "kubesphere.io/api/application/v2"
 )
 
-func TestLoadRepoIndexFormOci(t *testing.T) {
+func TestLoadRepoIndexFromOci(t *testing.T) {
 
 	url := "oci://localhost:5000/shipper-artifacts/demo/test02/helm-gen"
 	cred := appv2.RepoCredential{
 		Username: "",
 		Password: "",
 	}
-	index, err := LoadRepoIndexFormOci(url, cred)
+	index, err := LoadRepoIndexFromOci(url, cred)
 	if err != nil {
-		t.Errorf("LoadRepoIndexFormOci() error: %s", err)
+		t.Errorf("LoadRepoIndexFromOci() error: %s", err)
 	}
 	t.Log(len(index.Entries))
 }
