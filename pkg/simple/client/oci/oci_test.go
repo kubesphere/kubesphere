e6238f17b2e36709737a33c3d558fc2bd0f76760
lingbo <lingbo@lingbohome.com>
2024-09-24 22:59:59 +0800

    update oci_test.go
    Signed-off-by: lingbo <lingbo@lingbohome.com>

--- pkg/simple/client/oci/oci_test.go
+++ pkg/simple/client/oci/oci_test.go
@@ -2,6 +2,11 @@ package oci
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 	"time"
@@ -10,7 +15,47 @@ import (
 )
 
 func TestRegistry_Api(t *testing.T) {
-	reg, err := NewRegistry("registry.cn-shanghai.aliyuncs.com",
+	testRepos := []string{"helmcharts/nginx", "helmcharts/test-api", "helmcharts/test-ui", "helmcharts/demo-app"}
+	testTags := []string{"1.0.0", "1.2.0", "1.0.3"}
+	testRepo := testRepos[1]
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet && (r.URL.Path == "/v2" || r.URL.Path == "/v2/") {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		if r.Method == http.MethodGet && r.URL.Path == "/v2/_catalog" {
+			result := struct {
+				Repositories []string `json:"repositories"`
+			}{
+				Repositories: testRepos,
+			}
+			if err := json.NewEncoder(w).Encode(result); err != nil {
+				t.Errorf("failed to write response: %v", err)
+			}
+			return
+		}
+		if r.Method == http.MethodGet && r.URL.Path == fmt.Sprintf("/v2/%s/tags/list", testRepo) {
+			result := struct {
+				Tags []string `json:"tags"`
+			}{
+				Tags: testTags,
+			}
+			if err := json.NewEncoder(w).Encode(result); err != nil {
+				t.Errorf("failed to write response: %v", err)
+			}
+			return
+		}
+
+		t.Errorf("unexpected access: %s %s", r.Method, r.URL)
+		w.WriteHeader(http.StatusNotFound)
+	}))
+	defer ts.Close()
+	uri, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Fatalf("invalid test http server: %v", err)
+	}
+
+	reg, err := NewRegistry(uri.Host,
 		WithTimeout(5*time.Second),
 		WithBasicAuth("", ""),
 		WithInsecureSkipVerifyTLS(true))
@@ -26,7 +71,7 @@ func TestRegistry_Api(t *testing.T) {
 
 	var registryTags []string
 
-	repo, err := reg.Repository(ctx, "kube-shipper/shipper-ui")
+	repo, err := reg.Repository(ctx, testRepo)
 	if err != nil {
 		t.Fatalf("Registry.Repository() error = %v", err)
 	}
