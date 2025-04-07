5e700d46933d8a0840c9d9533936fabd5d256211
Yunkang Ren <33660223+renyunkang@users.noreply.github.com>
2025-04-07 10:27:19 +0800

    feat: move ks-core agent installer to job (#6473)
    Signed-off-by: renyunkang <rykren1998@gmail.com>

--- pkg/controller/cluster/helper.go
+++ pkg/controller/cluster/helper.go
@@ -7,17 +7,17 @@ package cluster
 
 import (
 	"context"
-	"errors"
-	"time"
+	"fmt"
+	"os"
+	"path"
 
-	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/chart/loader"
-	"helm.sh/helm/v3/pkg/storage/driver"
+	"helm.sh/helm/v3/pkg/chartutil"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog/v2"
 	clusterv1alpha1 "kubesphere.io/api/cluster/v1alpha1"
-	"kubesphere.io/utils/helm"
 	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/yaml"
 
@@ -43,74 +43,6 @@ func setConfigHash(cluster *clusterv1alpha1.Cluster) {
 	}
 }
 
-func installKSCoreInMemberCluster(kubeConfig []byte, jwtSecret, chartPath string, chartConfig []byte) error {
-	helmConf, err := helm.InitHelmConf(kubeConfig, constants.KubeSphereNamespace)
-	if err != nil {
-		return err
-	}
-
-	if chartPath == "" {
-		chartPath = "/var/helm-charts/ks-core"
-	}
-	chart, err := loader.Load(chartPath) // in-container chart path
-	if err != nil {
-		return err
-	}
-
-	// values example:
-	// 	map[string]interface{}{
-	//		"nestedKey": map[string]interface{}{
-	//			"simpleKey": "simpleValue",
-	//		},
-	//  }
-	values := make(map[string]interface{})
-	if chartConfig != nil {
-		if err = yaml.Unmarshal(chartConfig, &values); err != nil {
-			return err
-		}
-	}
-
-	// Override some necessary values
-	values["role"] = "member"
-	values["multicluster"] = map[string]string{"role": "member"}
-	// disable upgrade to prevent execution of kse-upgrade
-	values["upgrade"] = map[string]interface{}{
-		"enabled": false,
-	}
-	if err = unstructured.SetNestedField(values, jwtSecret, "authentication", "issuer", "jwtSecret"); err != nil {
-		return err
-	}
-
-	helmStatus := action.NewStatus(helmConf)
-	if _, err = helmStatus.Run(releaseName); err != nil {
-		if !errors.Is(err, driver.ErrReleaseNotFound) {
-			return err
-		}
-
-		// the release not exists
-		install := action.NewInstall(helmConf)
-		install.Namespace = constants.KubeSphereNamespace
-		install.CreateNamespace = true
-		install.Wait = true
-		install.ReleaseName = releaseName
-		install.Timeout = time.Minute * 5
-		if _, err = install.Run(chart, values); err != nil {
-			return err
-		}
-		return nil
-	}
-
-	upgrade := action.NewUpgrade(helmConf)
-	upgrade.Namespace = constants.KubeSphereNamespace
-	upgrade.Install = true
-	upgrade.Wait = true
-	upgrade.Timeout = time.Minute * 5
-	if _, err = upgrade.Run(releaseName, chart, values); err != nil {
-		return err
-	}
-	return nil
-}
-
 func getKubeSphereConfig(ctx context.Context, client runtimeclient.Client) (*config.Config, error) {
 	cm := &corev1.ConfigMap{}
 	if err := client.Get(ctx, types.NamespacedName{Name: constants.KubeSphereConfigName, Namespace: constants.KubeSphereNamespace}, cm); err != nil {
@@ -123,11 +55,62 @@ func getKubeSphereConfig(ctx context.Context, client runtimeclient.Client) (*con
 	return configData, nil
 }
 
-func hasCondition(conditions []clusterv1alpha1.ClusterCondition, conditionsType clusterv1alpha1.ClusterConditionType) bool {
-	for _, condition := range conditions {
-		if condition.Type == conditionsType && condition.Status == corev1.ConditionTrue {
-			return true
+// generateChartValueBytes generates the chart value bytes for the cluster
+func generateChartValueBytes(chartConfig []byte, jwtSecret string) ([]byte, error) {
+	values := make(map[string]interface{})
+	if chartConfig != nil {
+		if err := yaml.Unmarshal(chartConfig, &values); err != nil {
+			return nil, err
 		}
 	}
-	return false
+
+	// Override some necessary values
+	values["role"] = "member"
+	values["multicluster"] = map[string]string{"role": "member"}
+	// disable upgrade to prevent execution of kse-upgrade
+	values["upgrade"] = map[string]interface{}{
+		"enabled": false,
+	}
+	if err := unstructured.SetNestedField(values, jwtSecret, "authentication", "issuer", "jwtSecret"); err != nil {
+		return nil, err
+	}
+
+	valuesBytes, err := yaml.Marshal(values)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal values: %v", err)
+	}
+	return valuesBytes, nil
+}
+
+func getChartBytes(chartPath string) ([]byte, error) {
+	prefix := "/var/helm-charts"
+	if chartPath == "" {
+		chartPath = path.Join(prefix, releaseName)
+	}
+
+	tgzFile := path.Join(prefix, fmt.Sprintf("%s.tgz", releaseName))
+	if _, err := os.Stat(tgzFile); os.IsNotExist(err) {
+		chart, err := loader.Load(chartPath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load chart: %v", err)
+		}
+
+		saveFile, err := chartutil.Save(chart, prefix)
+		if err != nil {
+			return nil, fmt.Errorf("failed to save chart: %v", err)
+		}
+
+		klog.Infof("saveFile %s, tgzFile %s", saveFile, tgzFile)
+		if saveFile != tgzFile {
+			if err := os.Rename(saveFile, tgzFile); err != nil {
+				return nil, fmt.Errorf("failed to rename chart file: %v", err)
+			}
+		}
+	}
+
+	chartBytes, err := os.ReadFile(tgzFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read chart files: %v", err)
+	}
+	return chartBytes, nil
 }
